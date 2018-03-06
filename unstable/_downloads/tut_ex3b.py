a9dd41b6b4b66d1ab288470d1468356b94cdea07
Flatiron Jenkins <jenkins@flatironinstitute.org>
2018-03-06 18:12:25 -0500

    Generated documentation for unstable
    af198e8b47d8ddea1771a215d6007bede221f8ba

---
+++ unstable/_downloads/tut_ex3b.py
@@ -0,0 +1,16 @@
+from pytriqs.gf import GfReFreq
+from pytriqs.archive import HDFArchive
+from math import pi
+
+R = HDFArchive('myfile.h5', 'r') 
+ 
+from pytriqs.plot.mpl_interface import oplot, plt
+
+for name, g in R.items() :  # iterate on the elements of R, like a dict ...
+    oplot( (- 1/pi * g).imag, "-o", name = name)
+
+plt.xlim(-1,1) 
+plt.ylim(0,7) 
+
+p.savefig("./tut_ex3b.png") 
+
