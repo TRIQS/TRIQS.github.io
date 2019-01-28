aac00c6800913ec2cf4144e7d4cec5f98f6e4bbb
Flatiron Jenkins <jenkins@flatironinstitute.org>
2019-01-28 14:26:48 -0500

    Generated documentation for cthyb/2.1.x
    jenkins-TRIQS-cthyb-2.1.x-1 b07bfe1c2e2fb4660a2caf4fc38acf8a0f663759

---
+++ cthyb/2.1.x/guide/dmft_plot.py
@@ -0,0 +1,11 @@
+from pytriqs.gf import *
+from pytriqs.archive import *
+from pytriqs.plot.mpl_interface import oplot, plt
+
+with HDFArchive("dmft_solution.h5",'r') as ar:
+    for i in range(5):
+        oplot(ar['G_iw-%i'%i]['up'], '-o', mode = 'I',
+              label = 'Iteration = %s'%i,
+              x_window = (0,2))
+
+plt.legend(loc=4)
