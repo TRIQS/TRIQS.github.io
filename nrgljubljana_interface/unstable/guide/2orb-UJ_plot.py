123ff3f4b163af489d1cba064e29bc9d383bf61b
Flatiron Jenkins <jenkins@flatironinstitute.org>
2020-02-12 02:22:19 -0500

    Generated documentation for nrgljubljana_interface/unstable
    jenkins-TRIQS-nrgljubljana_interface-unstable-131 2243aec268e86e923eb45b6be08c947d912f0af4

--- nrgljubljana_interface/unstable/guide/2orb-UJ_plot.py
+++ nrgljubljana_interface/unstable/guide/2orb-UJ_plot.py
@@ -12,22 +12,13 @@ def A_to_nparrays(A):
     ly = np.array(A.data[:,0,0].real)
     return lx, ly
 
-def save_A(fn, gf):
-  f = open(fn, "w")
-  for w in gf.mesh:
-    z = gf[w][0,0]
-    f.write("%f %f\n" % (w, -1.0/math.pi*z.imag))
-
 with HDFArchive('2orb-UJ_solution.h5','r') as ar:
     # Expectation values
     print("<n>=",ar['expv']['n_d1'])
     print("<n^2>=",ar['expv']['n_d1^2'])
-
-    save_A("A_w_NRG.dat", ar['G_w']['imp'])
 
     # Spectral function
     A_w = ar['A_w']['imp']
     lx, ly = A_to_nparrays(A_w)
     plt.plot(lx, ly)
     plt.show()
-    plt.savefig("plot.pdf")
