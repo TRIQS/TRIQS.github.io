d4636755284783b5e68531ad0e27e6c36cf57ba0
Flatiron Jenkins <jenkins@flatironinstitute.org>
2025-09-19 18:53:03 -0400

    Generated documentation for modest/unstable
    jenkins-TRIQS-modest-unstable-119 9b7989220f2b06574946f906acb0ec8a66bcc31b

--- modest/unstable/doxygen/search/functions_0.js
+++ modest/unstable/doxygen/search/functions_0.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['ainv_5fb_0',['Ainv_B',['../namespacenda.html#ae87d23749dea2acc2ff577d301cfacb4',1,'nda']]],
   ['all_5fequal_1',['all_equal',['../namespacetriqs.html#ae7a23811d0a2f389f3248d3cf9b022b3',1,'triqs']]],
-  ['analyze_5fdegenerate_5fblocks_2',['analyze_degenerate_blocks',['../group__deg.html#gaeb151006691b19a21e2bb56b22045fd9',1,'triqs::modest']]],
+  ['analyze_5fdegenerate_5fblocks_2',['analyze_degenerate_blocks',['../group__deg.html#gaa30bb9bc642b8e84df648b35a98b7d80',1,'triqs::modest::analyze_degenerate_blocks(block_gf&lt; Mesh, matrix_valued &gt; const &amp;Gimp, double threshold=1.e-5)'],['../namespacetriqs_1_1modest.html#acc1e171e4f8ffc6f274486a7ba14103e',1,'triqs::modest::analyze_degenerate_blocks(block_gf&lt; dlr_imfreq, matrix_valued &gt; const &amp;G, double threshold)'],['../namespacetriqs_1_1modest.html#a4aabc6ff1a66999aae1fda0140df2907',1,'triqs::modest::analyze_degenerate_blocks(block_gf&lt; dlr_imtime, matrix_valued &gt; const &amp;G, double threshold)'],['../namespacetriqs_1_1modest.html#ab51a578320417e1fbf2db31097f5193c',1,'triqs::modest::analyze_degenerate_blocks(block_gf&lt; imfreq, matrix_valued &gt; const &amp;G, double threshold)'],['../namespacetriqs_1_1modest.html#a45220e0a15039ce6ffc40867cf10ec27',1,'triqs::modest::analyze_degenerate_blocks(block_gf&lt; imtime, matrix_valued &gt; const &amp;G, double threshold)']]],
   ['append_3',['append',['../classtriqs_1_1modest_1_1checkpoint.html#a1de10edbb0c75304ca7c381e55d761a6',1,'triqs::modest::checkpoint']]],
   ['as_4',['as',['../classh5_1_1proxy.html#ad0c48a6cdfe531bf85ed81170a3f151a',1,'h5::proxy']]],
   ['assert_5fis_5fgroup_5',['assert_is_group',['../classh5_1_1proxy.html#a41bf00ee01ee202b195b1b00b2ab694e',1,'h5::proxy']]],
